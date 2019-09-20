4ec051473c16da68066d12aec82bf5917fa6c9c6
zhengxinqi <zhengxinqi@localhost.localdomain>
2019-09-20 02:25:41 +0000

    cartlist

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-    Route::get('Goods/GoodsCart','Goods\\CartListController@index');
+    Route::get('Goods/GoodsCart','Goods\\CartListController@index');//购物车列表页
+    Route::get('Goods/CartListDel','Goods\\CartListController@cartListDel');//购物车列表删除
+    Route::get('Goods/CartListNum','Goods\\CartListController@CartListNum');//购物车列表购买数量
 
 
     Route::get('Goods/GoodsList','Goods\\GoodsListController@index');//商品首页
-    Route::get('Goods/GoodsDetail','Goods\\GoodsDetailController@goodsDetail');   //商品详情
+    Route::get('Goods/GoodsDetail','Goods\\GoodsDetailController@goodsDetail');//商品详情
