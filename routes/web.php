72822bf3a240bfd362a2e7b83e3e03fc5150df34
jmx <jmx@localhost.localdomain>
2019-09-23 09:20:31 +0000

    跳转地址修改

--- routes/web.php
+++ routes/web.php
@@ -20,18 +20,17 @@ Route::prefix('/Goods')->middleware('Login')->group(function(){
     Route::get('CartListDel','Goods\\CartListController@cartListDel');//购物车列表删除
     Route::get('CartListNum','Goods\\CartListController@CartListNum');//购物车列表购买数量
 
-<<<<<<< HEAD
-    Route::get('GoodsList','Goods\\GoodsListController@index');//商品首页
-    Route::get('GoodsDetail','Goods\\GoodsDetailController@goodsDetail');//商品详情
+    Route::get('goodsDetail','Goods\\GoodsDetailController@goodsDetail');//商品详情
     Route::get('GoodsCart','Goods\\CartListController@index');
-    Route::get('GoodsList','Goods\\GoodsListController@index');//商品首页
     Route::post('addCart','Goods\\GoodsDetailController@addCart');   //点击加入购物车
     Route::any('code',"LoginController@code"); 
     //Route::any('index',"LoginController@index");
-}); 
+});
+    Route::get('Goods/GoodsList','Goods\\GoodsListController@index');//商品首页
+
     Route::any('Goods/code',"LoginController@code");
-    Route::any('register',"LoginController@register");   
-    Route::any('Goods/registerdo',"LoginController@registerdo");  
+    Route::any('Goods/register',"LoginController@register");
+    Route::any('Goods/registerdo',"LoginController@registerdo");
     Route::any('Goods/login',"LoginController@login");
     Route::any('logindo',"LoginController@logindo");
     Route::any('registerdo',"LoginController@registerdo");
