5cb12637439e8d15c4d98f8374a6ebe269c1deac
jmx <jmx@localhost.localdomain>
2019-09-23 06:33:06 +0000

    日常修改

--- app/Http/Controllers/Goods/GoodsListController.php
+++ app/Http/Controllers/Goods/GoodsListController.php
@@ -10,19 +10,8 @@ class GoodsListController extends Controller
 {
     public function index()
     {
-
-
-        $goodsdata = \DB::table('g_goods')->get();
-//        dd($goodsdata);
-
         $page=4;
-        $goodsdata = \DB::table('g_goods')->paginate($page);
-//        dd($data);
-
-        $goods_model = new Goods;
-        $goodsdata = $goods_model->get();
-        $page=4;
-        $goodsdata = $goods_model->paginate($page);
+        $goodsdata = $goods_model::paginate($page);
         return view('Goods/GoodsList',['goodsdata'=>$goodsdata]);
     }
 }
