75f5881d2d9194a49a6b93922c9fff00de48152b
haojincheng <haojincheng@localhost.localdomain>
2019-09-23 02:41:22 +0000

    DB修改为模型

--- app/Http/Controllers/Goods/GoodsListController.php
+++ app/Http/Controllers/Goods/GoodsListController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers\Goods;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Model\Goods;
 
 class GoodsListController extends Controller
 {
     public function index()
     {
-        $goodsdata = \DB::table('g_goods')->get();
+        $goods_model = new Goods;
+        $goodsdata = $goods_model->get();
         $page=4;
-        $goodsdata = \DB::table('g_goods')->paginate($page);
+        $goodsdata = $goods_model->paginate($page);
         return view('Goods/GoodsList',['goodsdata'=>$goodsdata]);
     }
 }
